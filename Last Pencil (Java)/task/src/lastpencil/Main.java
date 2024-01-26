267897771cb772a069ff7ff89f1ccb6e9f938f0e
Pavol Cuninka <pavol.cuninka@icloud.com>
2024-01-26 13:17:39 +0100

    stage2 comleted

--- Last Pencil (Java)/task/src/lastpencil/Main.java
+++ Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -1,22 +1,23 @@
 package lastpencil;
-
-import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        printPencilAndTurn();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many pencils would you like to use:");
+        int numberOfPencils = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Who will be the first (John, Jack):");
+        String firstPlayer = scanner.nextLine();
+
+        printPencilAndTurn(numberOfPencils, firstPlayer);
     }
 
-    public static void printPencilAndTurn() {
-        Random random = new Random();
-        int numberOfPencils = random.nextInt(10) + 1;
-        StringBuilder pencils = new StringBuilder();
+    public static void printPencilAndTurn(int numberOfPencils, String firstPlayer) {
 
-        for (int i = 0; i < numberOfPencils; i++) {
-            pencils.append("|");
-        }
-
-        System.out.println(pencils);
-        System.out.println("Your turn!");
+        System.out.println("|".repeat(Math.max(0, numberOfPencils)));
+        System.out.println(firstPlayer + " is going first!");
     }
 }
